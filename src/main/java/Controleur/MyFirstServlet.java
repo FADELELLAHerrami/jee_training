e7a98c4203caa21c9788af2a0d8fa959b99ca384
erramifadelellah && ayman eljamaaoui <fadelellaherrami@gmail.com>
2023-02-25 21:26:12 +0100

    req 2

--- src/main/java/Controleur/MyFirstServlet.java
+++ src/main/java/Controleur/MyFirstServlet.java
@@ -18,8 +18,11 @@ public class MyFirstServlet extends HttpServlet{
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		String ville = "Marseille";
-		req.setAttribute("maVille", ville);
+		Personne perso = new Personne();
+		perso.setNum(100);
+		perso.setNom("ERRAMI");
+		perso.setPrenom("Fadel ellah");
+		req.setAttribute("Personne", perso);
 		req.getRequestDispatcher("/WEB-INF/Vue.jsp").forward(req, res);
 		// getRequestDispatcher("/WEB-INF/vue.jsp") : permet d’indiquer l’emplacement de la vue et de la recupérer
 		// forward(request, response) : pour envoyer la requete et la r ˆ eponse (on les utilisera plus tard). ´
--- src/main/webapp/WEB-INF/Vue.jsp
+++ src/main/webapp/WEB-INF/Vue.jsp
@@ -1,7 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
  <%@ page import ="java.util.Date" %>
- 
+ <%@ page import ="Modele.Personne" %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -11,8 +11,8 @@
 <body>
 	<p>Hello from Essaouira</p>
 	<%
-		String maVille = (String) request.getAttribute("maVille");
-		out.println("Bienvenue à notre "+maVille);
+		Personne perso = (Personne) request.getAttribute("Personne");
+		out.println("Bonjour votre prénom :"+perso.getPrenom()+" ,votre nom est "+ perso.getNom()+" ,votre numéro est "+perso.getNum());
 	%> 
 </body>
 </html>
